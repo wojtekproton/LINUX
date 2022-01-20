5ef935683d467db5d91030989d978dd35c8b530b
Wojciech Kubiak <Wojciech.Kubiak@nordcloud.com>
2022-01-20 17:34:13 +0100

    main grow itself

--- mfa-auth.sh
+++ mfa-auth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copyrights Wojtek Kubiak
-# Version 1.0
+# Version 1.0 (main continue to grow)
 # Default filename values - change this or add as environment values, depending on your own needs
 MFA_SERIAL_FILE=".mfaserial"
 AWS_TOKEN_FILE=".awstoken"
