80682f52be061c035c2cc7cb5ee9c105eb92b69a
Wojciech Kubiak <Wojciech.Kubiak@nordcloud.com>
2022-01-27 00:14:16 +0100

    working on MAC

--- mfa-auth.sh
+++ mfa-auth.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyrights Wojtek Kubiak
-# Version 1.1 (dev and main continue to grow as well)
+# Version 1.2 (dev and main continue to grow as well)
 # Default filename values - change this or add as environment values, depending on your own needs
 MFA_SERIAL_FILE=".mfaserial"
 AWS_TOKEN_FILE=".awstoken"
-TMP_DIR="${HOME}/.aws/TMP"
+TMP_DIR="${HOME}/.aws/CFG"
 CREDENTIALS_FILE="${HOME}/.aws/credentials"
 MFA_PROFILE="mfa"
 DURATION="900" #how long the token will work in seconds
@@ -19,8 +19,8 @@ fi
 # Validate that the configuration has been done before
 # If not, prompt the user to run that first
 if [ ! -e $CREDENTIALS_FILE ]; then
-	echo "First time configuration"
-	`aws configure`
+	echo "First time configuration:"
+	_out=`aws configure`
 fi
 
 # Validate that the MFA has been done before
@@ -57,37 +57,33 @@ promptForMFA(){
   _authenticationOutput=`aws sts get-session-token --duration-seconds ${DURATION} --serial-number ${_MFA_SERIAL} --token-code ${_MFA_TOKEN} --profile ${DEFAULT_PROFILE}`
   
   # Save authentication to some file
-  echo "AWS cmd: " 
-  echo "aws sts get-session-token --duration-seconds ${DURATION} --serial-number ${_MFA_SERIAL} --token-code ${_MFA_TOKEN} --profile ${DEFAULT_PROFILE}"
+  #echo "AWS cmd: " 
+  #echo "aws sts get-session-token --duration-seconds ${DURATION} --serial-number ${_MFA_SERIAL} --token-code ${_MFA_TOKEN} --profile ${DEFAULT_PROFILE}"
   
- # echo $_authenticationOutput > $TMP_DIR/$AWS_TOKEN_FILE;
-  `export AWS_PROFILE=${MFA_PROFILE}`
+  echo $_authenticationOutput > $TMP_DIR/$AWS_TOKEN_FILE;
 }
 
 # If token is present, retrieve it from file
 # Else invoke the prompt for mfa function
 if [ -e $TMP_DIR/$AWS_TOKEN_FILE ]; then
-	echo "Token exists in location $TMP_DIR/$AWS_TOKEN_FILE. Retreving the TOKEN"
+	#echo "Token exists in location $TMP_DIR/$AWS_TOKEN_FILE. Retreving the TOKEN"
   _authenticationOutput=`cat $TMP_DIR/$AWS_TOKEN_FILE`
   _authExpiration=`echo $_authenticationOutput | jq -r '.Credentials.Expiration'`
-  _nowTime=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
+  _nowTime=`date -u +'%Y-%m-%dT%H:%M:%S+00:00'`
   
   # Retrieving is not sufficient, since we are not sure if this token has expired
-  export AWS_SESSION_TOKEN=$_authenticationOutput
-  echo "Token: $AWS_SESSION_TOKEN"
-  echo "authExpiration: $_authExpiration"
+  #echo "Token: $AWS_SESSION_TOKEN"
+  t1=$(date -j -f "%Y-%m-%dT%H:%M:%S+00:00" "$_authExpiration" "+%s")
+  t2=$(date -j -f "%Y-%m-%dT%H:%M:%S+00:00" "$_nowTime" "+%s")
+  seconds_left=$((${t1} - ${t2}))
+  echo "Token valid for seconds: " $seconds_left
   # Check for the expiration value against the current time
   # If expired, invoke the prompt for mfa function
-  if [ "$_authExpiration" /< "$_nowTime" ]; then
+  if [[ $seconds_left -lt 0 ]]; then
     echo "Your last token has expired"
     promptForMFA
-#  else
-#	StartDate=$(date -u -d "$_authExpiration" +"%s")
-#	EndDate=$(date -u -d "$_nowTime" +"%s")
-#	token_expire_in=`date -u -d "0 $StartDate sec - $EndDate sec" +"%H:%M:%S"`
-#	echo "Token VALID for: ${token_expire_in}"
-#
   fi
+  
 else
   promptForMFA
 fi
@@ -95,38 +91,47 @@ fi
 # "Return" the values to the calling script.
 # There are a few ways to "return", for example writing to file
 # Here, we assume that this script is "sourced" - see more on "sourcing" here: https://bash.cyberciti.biz/guide/Source_command
-#_AWS_ACCESS_KEY_ID=`echo ${_authenticationOutput} | jq -r '.Credentials.AccessKeyId'`
-#_AWS_SECRET_ACCESS_KEY=`echo ${_authenticationOutput} | jq -r '.Credentials.SecretAccessKey'`
-#_AWS_SESSION_TOKEN=`echo ${_authenticationOutput} | jq -r '.Credentials.SessionToken'`
-#_AWS_EXPIRATION=`echo ${_authenticationOutput} | jq -r '.Credentials.Expiration'`
+_AWS_ACCESS_KEY_ID=`echo ${_authenticationOutput} | jq -r '.Credentials.AccessKeyId'`
+_AWS_SECRET_ACCESS_KEY=`echo ${_authenticationOutput} | jq -r '.Credentials.SecretAccessKey'`
+_AWS_SESSION_TOKEN=`echo ${_authenticationOutput} | jq -r '.Credentials.SessionToken'`
+_AWS_EXPIRATION=`echo ${_authenticationOutput} | jq -r '.Credentials.Expiration'`
 
 
+# Checking if $MFA_PROFILE exists in credential file
+isInFile=$(cat ${HOME}/.aws/credentials | grep -c "$MFA_PROFILE")
+
+if [ $isInFile -eq 0 ]; then
+  # Nie znalazlem MFA_PROFILE w pliku wiec dopisuje
+   echo "" >> ${HOME}/.aws/credentials
+   echo "[$MFA_PROFILE]" >> ${HOME}/.aws/credentials
+fi
 
 # Writing new vaules to credentials file
-#TMP_CRED_FILE="${HOME}/.aws/credentials_tmp"
-#skip=0
-#echo "# By Woku " > $TMP_CRED_FILE
+TMP_CRED_FILE="${HOME}/.aws/credentials_tmp"
+skip=0
+# File creation
+#echo "#By Woku" > $TMP_CRED_FILE
 
-#while IFS= read -r line
-#do
-#	if [[ $skip == 0 ]]; then
-#		echo "$line" >> $TMP_CRED_FILE
-#	else
-#		# waiting for blank line to continue coping from old credentials to tmp
-#		if [[ -z $line ]]; then
-#			echo "$line" >> $TMP_CRED_FILE
-#			skip=0
-#		fi	
-#	fi
-#	if [[ $line == "[$MFA_PROFILE]" ]]; then
-#		echo "aws_access_key_id =" $_AWS_ACCESS_KEY_ID >> $TMP_CRED_FILE
-#		echo "aws_secret_access_key =" $_AWS_SECRET_ACCESS_KEY >> $TMP_CRED_FILE
-#		echo "aws_session_token =" $_AWS_SESSION_TOKEN >> $TMP_CRED_FILE
-#		echo "aws_expiration =" $_AWS_EXPIRATION >> $TMP_CRED_FILE
-#		# Skip next rows until blank line appears
-#		skip=1
-#	fi
-#done < "${HOME}/.aws/credentials"
+while IFS= read -r line
+do
+	if [[ $skip == 0 ]]; then
+		echo "$line" >> $TMP_CRED_FILE
+	else
+		# waiting for blank line to continue coping from orginal credentials to tmp
+		if [[ -z $line ]]; then
+			echo "$line" >> $TMP_CRED_FILE
+			skip=0
+		fi	
+	fi
+	if [[ $line == "[$MFA_PROFILE]" ]]; then
+		echo "aws_access_key_id =" $_AWS_ACCESS_KEY_ID >> $TMP_CRED_FILE
+		echo "aws_secret_access_key =" $_AWS_SECRET_ACCESS_KEY >> $TMP_CRED_FILE
+		echo "aws_session_token =" $_AWS_SESSION_TOKEN >> $TMP_CRED_FILE
+		echo "aws_expiration =" $_AWS_EXPIRATION >> $TMP_CRED_FILE
+		# Skip next rows until blank line appears
+		skip=1
+	fi
+done < "${HOME}/.aws/credentials"
 
-#mv ${HOME}/.aws/credentials ${HOME}/.aws/TMP/.credentials_old
-#mv $TMP_CRED_FILE ${HOME}/.aws/credentials
+mv ${HOME}/.aws/credentials ${TMP_DIR}/.credentials_old
+mv $TMP_CRED_FILE ${HOME}/.aws/credentials
